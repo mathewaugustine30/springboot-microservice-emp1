295090c4fc3fec604fa601d6b4dbf619cc2d85f6
mathewaugustine30 <mathewaugustine30@gmail.com>
2021-09-07 16:21:28 +0530

    Adding Cart

---
+++ coupon-service-1/src/test/java/com/mathew/java/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathew.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
